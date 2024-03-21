3deec5c2270438f6043b0895d8e5386fc29b8e06
Doron-Breska <124248816+Doron-Breska@users.noreply.github.com>
2024-03-21 19:17:56 +0100

    added categories for skillModal

--- server/modals/skillModal.js
+++ server/modals/skillModal.js
@@ -1,6 +1,33 @@
 import mongoose from "mongoose";
 const { Schema } = mongoose;
 
+const CategoryEnum = [
+  "Software Development",
+  "Web Development",
+  "App Development",
+  "UX/UI Design",
+  "Data Science",
+  "Mathematics",
+  "Economics",
+  "Physics",
+  "Marketing",
+  "Photography",
+  "Electronic Music (DJing)",
+  "Playing Instruments",
+  "Singing",
+  "Sound Design",
+  "Interior Design",
+  "Photoshop",
+  "Drawing",
+  "Sculpture",
+  "Dance",
+  "Cooking",
+  "Languages",
+  "Working Out/Sports",
+  "Creative Writing",
+  "Script Writing",
+];
+
 const SkillSchema = new Schema(
   {
     owner: {
@@ -13,6 +40,7 @@ const SkillSchema = new Schema(
       type: String,
       required: true,
       index: true,
+      enum: CategoryEnum, // Use the enum for category
     },
     description: {
       type: String,
